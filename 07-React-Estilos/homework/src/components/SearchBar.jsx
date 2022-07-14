fcb93264498a93f932d4a4c9f1fd88a20de00bcc
Juan Brusasca <juanbrusasca96@gmail.com>
2022-07-14 15:10:56 -0300

    lista homework 07

--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from '../styles/SearchBar.module.css'
 
-export default function SearchBar({onSearch}) {
+export default function SearchBar({ onSearch }) {
   // acá va tu código
-  return (<div className={styles.divSearchBar}>
-    <input className={styles.input} type="text" placeholder='Ciudad...'/>
-    <button className={styles.boton} onClick={onSearch}>Agregar</button>
-  </div>)
+  return (
+    <div className={styles.divSearchBar}>
+      <input className={styles.input} type="text" placeholder='Ciudad...' />
+      <button className={styles.boton} onClick={onSearch}>Agregar</button>
+    </div>
+  )
 };
